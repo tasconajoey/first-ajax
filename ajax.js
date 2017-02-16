166eaacf1a593d3a345f8f4700b344c6291f2d7e
tasconajoey <joey.tascona@gmail.com>
2017-02-16 17:03:12 -0500

    Done

--- ajax.js
+++ ajax.js
@@ -11,15 +11,16 @@ $(document).ready(function () {
   });
   $('#step3456 button').on('click', function() {
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/ping',
+      url: 'http://first-ajax-api.herokuapp.com/pong',
       method: 'GET',
       dataType: 'text'
     }).done(function(responseData) {
       console.log(responseData);
       console.log('The request succeeded!');
       $('#step3456').append(responseData);
-    }).fail(function() {
-      $('#step3456').append('We\'ll try harder next time!');
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      $('body').append(jqXHR.responseText);
+      // $('body').append(jqXHR);
       console.log('The request failed!');
     }).always(function() {
       console.log('Hey the request finished!');
@@ -29,16 +30,34 @@ $(document).ready(function () {
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/count',
       method: 'GET',
+      data: { amount: 5 },
       dataType: 'text'
     }).done(function(responseData) {
       $('#step7').append(responseData);
     });
   });
-  $('#step8 button').on('click', function() {
+  $('#submit').on('click', function() {
+    var x = document.getElementById('text').value;
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/time',
       method: 'GET',
-      data: { timezone: 'Asia/Kolkata' },
+      data: { timezone: x },
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log('done');
+      $('#step8').append(responseData);
+    }).fail(function() {
+      $('#step8').append('ERROR');
+      console.log('fail');
+    }).always(function() {
+      console.log('always');
+    });
+  });
+  $('#step8 #sub').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: { timezone: 'Europe/Sofia' },
       dataType: 'text'
     }).done(function(responseData) {
       $('#step8').append(responseData);
